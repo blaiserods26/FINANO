9e2332c66a9c6e20e30d993b4736cf1b5a9e09bd
blaiserods26 <blaiserods26@gmail.com>
2024-02-19 11:23:28 +0530

    link

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,21 +1,3 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "firebase/app";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
-
- // Your web app's Firebase configuration
- const firebaseConfig = {
- apiKey: "AIzaSyD_sZko3ajzY8dF71vErR_qMq7v9SmDIr0",
- authDomain: "finano-9812.firebaseapp.com",
- databaseURL: "https://finano-9812-default-rtdb.asia-southeast1.firebasedatabase.app",
- projectId: "finano-9812",
- storageBucket: "finano-9812.appspot.com",
- messagingSenderId: "300059984926",
- appId: "1:300059984926:web:b9b1b1dac0c57bc8c35a0f"
- };
-
- // Initialize Firebase
- const app = initializeApp(firebaseConfig);
 
 
 (function($) {		
